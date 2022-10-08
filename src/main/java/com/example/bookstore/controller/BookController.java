5e166f041c44328f2a3d7b70f5b0ce57bd68e29e
taohui <htao@thoughtworks.com>
2022-10-08 11:10:59 +0800

    taohui init bookController

---
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.example.bookstore.controller;
+
+import com.example.bookstore.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks() {
+        BookDto bookDto = BookDto.builder().title("title").build();
+
+        List<BookDto> books = new ArrayList<>();
+        books.add(bookDto);
+
+        return ResponseEntity.ok(books);
+
+    }
+
+}
